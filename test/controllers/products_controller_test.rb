af3d558807db31ef0c26e4502cef670cc9a0b039
Irene DeVera <irenneeyyy@gmail.com>
2017-10-18 15:29:32 -0700

    Started products status

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "returns success when showing all products" do
+      get products_ppath
+
+      must_respond_with :success
+    end
+
+    #TODO: Need to make an alternative
+
+    it "returns success when a product show page" do
+    end
+  end
 end
