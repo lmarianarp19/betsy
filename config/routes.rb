0cf45ec2661c7e6acd94524476f00930e727ef75
Mariana <lmarianarp@gmail.com>
2017-10-17 18:11:45 -0700

    Adding some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#root'
+
+  resources :merchants, only: [:show]
+
+  resources :products
+
+  resources :categories, only:[:index, :new, :create]
+
+  resources :bills, :only[:index, :new, :create, :show]
+
+  resources :orders, :only[:show, :create, :update]
+
+  resources :reviews, :only[:new, :create, :index, :show]
+
 end
