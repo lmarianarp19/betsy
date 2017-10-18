a8dcc4410f5545bb05f99aa2b805e5276dd89444
Irene DeVera <irenneeyyy@gmail.com>
2017-10-18 15:04:24 -0700

    add tests to review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,12 +7,12 @@ class ReviewsController < ApplicationController
   def create # REFACTOR FLASH MESSAGES IF NEEDED
     @review = Review.new(reviews_params)
 
-    if @review
-      flash[:success] = :success
+    if @review.save!
+      flash[:status] = :success
       flash[:message] = "Thank you for reviewing this product!"
-      redirect_to product_path(@review.product.id) # Product View Page
+      redirect_to product_path(@review.product_id) # Product View Page
     else
-      flash[:success] = :failure
+      flash[:status] = :failure
       flash[:message] = "Could not submit review. Please try again!"
       flash[:errors] = @review.errors.messages
       render :new, status: :bad_request
@@ -23,7 +23,7 @@ class ReviewsController < ApplicationController
   private
 
   def reviews_params
-    params.require(:review).permit(:description, :rating)
+    params.require(:review).permit(:review, :rating, :product_id)
   end
 
 end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 describe ReviewsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "new" do
+    it "returns success when making a new review" do
+      get new_review_path
+
+      must_respond_with :success
+    end
+  end
+
+  #TODO: Think of a false case for new
+
+  describe "create" do
+    it "returns success when saving a review" do
+      before_count = Review.count
+
+      review_data = {
+        review: {
+          review: "This is another review that will be created",
+          rating: 1,
+          product_id: Product.first.id
+        }
+      }
+
+      Review.new(review_data[:review]).must_be :valid?
+
+      post reviews_path, params: review_data
+      #
+      must_respond_with :redirect
+      # must_redirect_to product_path(review_data[:review][:product_id])
+      # Review.count.must_equal before_count + 1
+    end
+
+  end
+
 end
