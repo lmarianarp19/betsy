5a0db02609a6baefdbd4a98423b87ad8d50f029d
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-10-19 09:20:33 -0700

    Add seed data for review, add model test for review, change to review fixture data, and removed git code from category model file

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ApplicationRecord
 
+  has_many :product_categories
+  has_many :products, through: :product_categories
+
   def self.find_or_create_cat(category_name)
   existing_category = Category.find_by(name: category_name.upcase)
 
@@ -19,8 +22,5 @@ class Category < ApplicationRecord
 
     end
   end
-=======
-  has_many :product_categories
-  has_many :products, through: :product_categories
->>>>>>> master
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'csv'
+
+REVIEW_FILE = Rails.root.join('db', 'review.csv')
+puts "Loading raw media data from #{REVIEW_FILE}"
+
+review_failures = []
+CSV.foreach(REVIEW_FILE, :headers => true) do |row|
+  review = Review.new
+  review.review = row['review']
+  review.rating = row['rating']
+  review.prodcut_id = row['product_id']
+  puts "Created review: #{review.inspect}"
+  successful = review.save
+  if !successful
+    review_failures << review
+  end
+end
+
+puts "Added #{Review.count} review records"
+puts "#{review_failures.length} reviews failed to save"
