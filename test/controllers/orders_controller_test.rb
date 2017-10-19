8c65caaf3ab5c26465ed61dd83749bdd2c29cfd8
Mariana <lmarianarp@gmail.com>
2017-10-19 13:20:37 -0700

    add view for show order and test for show

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 describe OrdersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "show" do
+    it "succeeds for an order that exists" do
+        order_id = Order.first.id
+        get order_path(order_id)
+        must_respond_with :success
+      end
+  end
 end
