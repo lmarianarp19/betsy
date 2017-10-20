2b80daf0c72cf7f506025148f7a252ab40196010
Irene DeVera <irenneeyyy@gmail.com>
2017-10-19 20:56:29 -0700

    Add category id to migration testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019235547) do
+ActiveRecord::Schema.define(version: 20171020035447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20171019235547) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
+    t.bigint "category_id"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
   end
 
