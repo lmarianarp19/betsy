4a6ba9fd62cc9a7012ecb434a13276defbac5f3f
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-19 22:53:43 -0700

    Adds validation for product catefory using scope

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,6 +1,10 @@
 class ProductCategory < ApplicationRecord
+  # relationships
   belongs_to :product
   belongs_to :category
+  
+  # validations
+  validates_uniqueness_of :product_id, :scope => :category_id
 
   def self.create_prod_cat(product)
     new_pc = ProductCategory.new(product_id: product[:product_id], category_id: product[:category_id])
