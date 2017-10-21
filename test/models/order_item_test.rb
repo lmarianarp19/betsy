aed3863fadbd96b2705c4ba1ad5cb287f491d596
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-21 12:18:32 -0700

    added some tests stubs to order_item tests

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -3,7 +3,35 @@ require "test_helper"
 describe OrderItem do
   let(:order_item) { OrderItem.new }
 
-  it "must be valid" do
-    value(order_item).must_be :valid?
+  describe "relations" do
+    it "has a product" do
+
+    end
+
+    it "has a order" do
+
+    end
+  end
+
+  describe "validations" do
+    it "allows one product to have many orders" do
+
+    end
+
+    it "allows one order to have multiple products" do
+
+    end
+
+    it "it doesn't allow the same product to appear twice in an order" do
+
+    end
+
+    it "requires a quantity" do
+
+    end
+
+    it "quantity must be a number and must be greater than 0" do
+
+    end
   end
 end
