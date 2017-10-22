169c1ba217144b404156f60adffaab1ec74a2b8b
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-10-22 12:57:53 -0700

    show method in controller, add cart route and add show page for cart

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,9 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
 
+  #add resource not resources because a customer will only have one shopping cart and doesn't create an index route 
   resource :cart, only: [:show]
-  
+
   resources :categories, only:[:index, :show, :new, :create]
 
   get 'categories/:id/products', to: 'products#index', as: 'products_categories'
