d4a04cc15a023929b5ffd9f281942720dd7732c8
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-23 11:34:55 -0700

    Stubbed out tests for categories model

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,32 @@
 require "test_helper"
 
 describe Category do
-  let(:category) { Category.new }
+  let(:first_catagory) {categories :fruit_category}
 
-  it "must be valid" do
-    value(category).must_be :valid?
+  describe "relations" do
+    it "has a collection of products" do
+      first_catagory.must_respond_to :products
+      first_catagory.products.each do |product|
+        product.must_be_kind_of Product
+      end
+    end
+    it "has a collection of product_categories" do
+
+    end
+  end
+
+  describe "validations" do
+    it "requires a name" do
+
+    end
+    it "requres that the name be unique" do
+
+    end
+  end
+
+  describe "methods" do
+    describe "create_cat" do
+
+    end
   end
 end
