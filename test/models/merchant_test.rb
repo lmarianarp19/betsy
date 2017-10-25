086ba671845bcdc1627c342974d068491c4322dd
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-25 10:55:38 -0700

    Again stupid error where I need to branch

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 describe Merchant do
   let(:merchant_one) { merchants(:first_merchant) }
   let(:merchant_two) { merchants(:second_merchant) }
+  let(:merchant_new) {Merchant.new}
 
   describe "valid" do
     it "will return false without a username" do
@@ -34,8 +35,40 @@ describe Merchant do
         product.must_be_kind_of Product
       end
     end
-    it "" do
+    it "has a list of order_items" do
+      merchant_new.orders.must_equal []
+    end
+    it "has a list of orders" do
 
     end
+  end
+
+  describe "Custom methods" do
+    describe "distinct_orders" do
+      it "will return an array of orders for a merchant that has orders" do
+
+      end
+
+      it "will return an array or orders where each order is unique" do
+
+      end
+
+      it "will return an empty array for a Merchant with no orders" do
+
+      end
+    end
+
+    describe "from_auth_hash" do
+
+    end
+
+    describe "orders_hash" do
+
+    end
+
+    describe "sum_ord_hash" do
+
+    end
+
   end
 end
