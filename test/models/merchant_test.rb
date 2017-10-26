6d35702845004543b1e54c16a3618779a10159fe
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-25 21:18:39 -0700

    Updates merchants model test to cover added relationships and custom methods

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -78,14 +78,22 @@ describe Merchant do
       end
     end
 
-    # describe "from_auth_hash" do
-    #   # no idea how to do this, doesnt seem important give our other priorities.
-    # end
-
     describe "orders_hash" do
-      it "for a user with orders it returns a hash where the keys are orders and the values are order items" do
+      it "for a user with orders it returns a hash where the keys are orders and the values are order items that belong the merchant" do
+        # hash
         o_hash = merchant_one.orders_hash
         o_hash.must_be_kind_of Hash
+
+        # keys are orders
+        o_hash.keys.each do |order|
+          order.must_be_kind_of Order
+        end
+
+        # values are order_items that belong to merchant
+        o_hash.values.flatten.each do |order_item|
+          order_item.must_be_kind_of OrderItem
+          order_item.merchant.must_equal merchant_one
+        end
       end
 
       it "returns an empty hash when no one has ordered a merchants products" do
@@ -94,12 +102,64 @@ describe Merchant do
     end
 
     describe "orders_hash_by_status" do
+      # this is basically the same as orders_hash except that you pass an order status through the method that acts as a filter
+      # success case user has orders and valid arg
+      it "will return a hash with only orders that match a valid status passed into method" do
+        merchant_one.orders_hash_by_status("complete").keys.each do |order|
+          order.status.must_equal "complete"
+        end
+        merchant_one.orders_hash_by_status("pending").keys.each do |order|
+          order.status.must_equal "pending"
+        end
+      end
 
+      it "returns an empty hash if a merchant has no orders" do
+        merchant_new.orders_hash_by_status("pending").must_equal Hash.new
+      end
+
+      it "returns an empty hash if the merchant doesn't have an order that matched the status argumetn passed into method" do
+        merchant_one.orders_hash_by_status("paid").must_equal Hash.new
+      end
+
+      it "returns an emtpy hash if call this method with invalid arguements for a merchant, with or without orders" do
+        invalid_arguments = [[1],1,Date.today,"yoda", merchant_one]
+        invalid_arguments.each do |bad_arg|
+          merchant_one.orders_hash_by_status(bad_arg).must_equal Hash.new
+          merchant_new.orders_hash_by_status(bad_arg).must_equal Hash.new
+        end
+      end
     end
-
     describe "sum_ord_hash" do
-
+      #success case, orders
+      it "will return the total marchant revenue for orders of a given status" do
+        # arrange
+        #find all a merchants order items for orders with a given status
+        m1_pend_ords = merchant_one.distinct_orders.where(status: "pending")
+        merchants_ois = []
+        m1_pend_ords.each do |order|
+          order.order_items.each do |oi|
+            if oi.merchant == merchant_one
+              merchants_ois << oi
+            end
+          end
+        end
+        # calculate expected revenue
+        expected = 0
+        merchants_ois.each do |oi|
+          expected += (oi.product.price * oi.quantity)
+        end
+        # act
+        actual = merchant_one.sum_ord_hash("pending")
+        # assert
+        expected.must_equal actual
+      end
+      it "will return 0 for merchants with no order or with any entry of invalid data" do
+        invalid_arguments = [[1],1,Date.today,"yoda", merchant_one]
+        invalid_arguments.each do |bad_arg|
+          merchant_one.sum_ord_hash(bad_arg).must_equal 0
+          merchant_new.sum_ord_hash(bad_arg).must_equal 0
+        end
+      end
     end
-
   end
 end
