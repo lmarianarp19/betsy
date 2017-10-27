7ef98c1165af00e8c2a7ceeb3bf2255899ffcc6d
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-26 17:25:57 -0700

    stubbed out test on merchant controller

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -5,48 +5,70 @@ describe MerchantsController do
   let(:merchant) { Merchant.first }
 
   describe "#show" do
-    it "returns success when showing a merchant show page" do
-      login(merchant)
-      merchant.must_be :valid?
+    describe "logged in merchant" do
+      it "returns success when showing a merchant show page" do
+        login(merchant)
+        merchant.must_be :valid?
 
-      get merchant_path(merchant)
+        get merchant_path(merchant)
 
-      must_respond_with :success
+        must_respond_with :success
+      end
+
+      it "returns success if the merchant has no summary of products" do
+        merchant.products.destroy_all
+        login(merchant)
+
+        get merchant_path(merchant)
+
+        must_respond_with :success
+      end
     end
 
-    it "returns success if the merchant has no summary of products" do
-      merchant.products.destroy_all
-      login(merchant)
+    describe "guest" do
+      it "returns redirect to root path if the merchant is not logged in" do
+        get merchant_path(merchant)
 
-      get merchant_path(merchant)
-
-      must_respond_with :success
-    end
-
-    it "returns redirect to root path if the merchant is not logged in" do
-      get merchant_path(merchant)
-
-      must_respond_with :redirect
-      must_redirect_to root_path
-      flash[:status] = :failure
+        must_respond_with :redirect
+        must_redirect_to root_path
+        flash[:status] = :failure
+      end
     end
   end
 
+
+
   describe "#products" do
-    it "returns success if the merchant has products" do
-      login(merchant)
+    describe "logged in merchant" do
+      it "returns success if the merchant has products" do
+        login(merchant)
 
-      get merchant_products_path(merchant)
+        get merchant_products_path(merchant)
 
-      must_respond_with :success
+        must_respond_with :success
+      end
+
+      # it "return success if the merchant has no current products" do
+      #   merchant.products.destroy_all
+      #
+      #   get merchant_product_path(merchant)
+      #
+      #   must_respond_with :success
+      # end
     end
-    #
-    # it "return success if the merchant has no current products" do
-    #   merchant.products.destroy_all
-    #
-    #   get merchant_product_path(merchant)
-    #
-    #   must_respond_with :success
-    # end
+
+    describe "guest" do
+      it "returns success if the merchant has products" do
+        get merchant_products_path(merchant)
+
+        must_respond_with :success
+      end
+
+      it "return success if the merchant has no products" do
+
+      end
+
+      it ""
+    end
   end
 end
